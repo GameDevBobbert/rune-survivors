bf34f7984025c20778cbfece242a4897344e3a7d
bobbert <robert.pallesen13@gmail.com>
2026-03-23 18:08:00 +0100

    add animatedsprite2d

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @onready var movement: MovementComponent = $MovementComponent
 @onready var health: HealthComponent = $HealthComponent
 @onready var combat: CombatComponent = $CombatComponent
+@onready var animated_sprite := $AnimatedSprite2D
 
 func _ready() -> void:
 	add_to_group("player")
@@ -11,6 +12,21 @@ func _ready() -> void:
 	
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+
+	if direction.length() > 0:
+		if abs(direction.x) > abs(direction.y):
+			# Horizontal movement
+			if direction.x > 0:
+				animated_sprite.play("right")
+			else:
+				animated_sprite.play("left")
+		else:
+			# Vertical movement
+			if direction.y > 0:
+				animated_sprite.play("down")
+			else:
+				animated_sprite.play("up")
+
 	movement.physics_update(self, direction, delta)
 
 func _process(delta: float) -> void:
